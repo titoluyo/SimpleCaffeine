7d66146527316c314fc1e61a1eac09199a348d19
Tito Luyo Murata <tluyo@hotmail.com>
2021-10-11 02:32:08 -0500

    initial commit

---
+++ SimpleCaffeine/SimpleCaffeine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleCaffeine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int seconds = 10;
+            var idleTimer = new Timer(TimerCallback, null, 0, seconds * 1000);
+            Console.ReadLine();
+        }
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("Proc: " + DateTime.Now);
+            Native.SetThreadExecutionState(
+                EXECUTION_STATE.ES_CONTINUOUS
+                | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                | EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+        }
+
+    }
+}
